279c32b61ccd9fe5f42d9a52df61ce3e7949f685
yuanshuo <yuanshuo_616@qq.com>
2025-12-29 10:41:55 +0800

    update: support save to local storage

--- src/index.js
+++ src/index.js
@@ -9,16 +9,13 @@ const initialize = (options, report = null) => {
         rb.load(report);
     }
     return rb;
-}
-(() => {
+};
+const emptyOptions = {styles: [], parameters: [], docElements: [], watermarks: []};
+const loadFromApi = (lang, project, user_id) => {
     fetchTemplate('purchase_order').then((response) => {
         let rb;
         if (response.data) {
             const report = response.data.report;
-            const url = new URL(window.location.href);
-            const project = url.searchParams.get('project');
-            const user_id = url.searchParams.get('user_id');
-            const lang = url.searchParams.get('lang');
             const reportBroOptions = {
                 reportServerUrl: `${baseURL}/report/run`,
                 menuShowButtonLabels: true,
@@ -27,7 +24,7 @@ const initialize = (options, report = null) => {
                     { name: '微软雅黑', value: 'MicrosoftYaHei'},
                 ],
                 // additionalFonts: [ { name: 'Tangerine', value: 'tangerine'}, { name: 'Lobster', value: 'lobster'} ],
-                saveCallback: function (report) {
+                saveCallback: function () {
                     console.log('Saved report', project, user_id, rb.getReport());
                     saveTemplate({
                         key: 'purchase_order',
@@ -54,5 +51,44 @@ const initialize = (options, report = null) => {
         alert('Error fetch init data');
         initialize({});
     });
+};
+const loadFromStorage = (lang) => {
+    const localData = localStorage.getItem('__localReport');
+    const report = localData ? JSON.parse(localData) : emptyOptions;
+    let rb;
+    let reportBroOptions = {};
+    try {
+        reportBroOptions = {
+            reportServerUrl: `${baseURL}/report/run`,
+            menuShowButtonLabels: true,
+            adminMode: true,
+            additionalFonts: [
+                { name: '微软雅黑', value: 'MicrosoftYaHei'},
+            ],
+            // additionalFonts: [ { name: 'Tangerine', value: 'tangerine'}, { name: 'Lobster', value: 'lobster'} ],
+            saveCallback: function () {
+                console.log('Saved report', rb.getReport());
+                localStorage.setItem('__localReport', JSON.stringify(rb.getReport()));
+                rb.setModified(false);
+            },
+            localeKey: lang === 'zh' ? 'zh_cn' : 'en_us',
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+    rb = initialize(reportBroOptions, report);
+};
+(() => {
+    const url = new URL(window.location.href);
+    const project = url.searchParams.get('project');
+    const user_id = url.searchParams.get('user_id');
+    const lang = url.searchParams.get('lang');
+    if (project && user_id) {
+        loadFromApi(lang, project, user_id);
+    } else {
+        loadFromStorage(lang);
+    }
+
 })();
 
